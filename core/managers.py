dc1ca26880c48fb1b896c7c5b7cedb15384bc999
OCCCAS <pavlov.timur556@yandex.ru>
2024-06-24 01:47:58 +0300

    feat: created create_superuser method

--- core/managers.py
+++ core/managers.py
@@ -21,4 +21,7 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(
         self, first_name: str, last_name: str, email: str, password: str, **extra_fields
     ):
-        return self.create_user(first_name, last_name, email, password, **extra_fields)
+        user = self.create_user(first_name, last_name, email, password, **extra_fields)
+        user.is_admin = True
+        user.save()
+        return user
