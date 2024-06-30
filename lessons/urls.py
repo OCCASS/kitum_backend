c4dd1d905b42bed6d5c28bd3f45500a100d4fc2b
OCCCAS <pavlov.timur556@yandex.ru>
2024-06-30 12:10:32 +0300

    refactor: urls names refactored

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,33 +3,33 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", LessonsView.as_view(), name="lessons"),
-    path("homework/", HomeworkLessons.as_view(), name="homework"),
-    path("<str:pk>/", LessonView.as_view(), name="lesson"),
+    path("", LessonsView.as_view(), name="lesson_list"),
+    path("homework/", HomeworkLessons.as_view(), name="homework_list"),
+    path("<str:pk>/", LessonView.as_view(), name="lesson_detail"),
     path(
         "<str:pk>/complete/",
         CompleteLessonView.as_view(),
-        name="complete_lesson",
+        name="lesson_complete",
     ),
     path(
         "<str:pk>/complete_tasks/",
         CompleteLessonTasksView.as_view(),
-        name="complete_lesson",
+        name="lesson_tasks_complete",
     ),
-    path("<str:pk>/skip/", SkipLessonView.as_view(), name="skip_lesson"),
+    path("<str:pk>/skip/", SkipLessonView.as_view(), name="lesson_skip"),
     path(
         "<str:pk>/<str:task_pk>/",
         LessonTaskView.as_view(),
-        name="task",
+        name="task_detail",
     ),
     path(
         "<str:pk>/<str:task_pk>/answer/",
         AnswerLessonTaskView.as_view(),
-        name="answer_task",
+        name="task_answer",
     ),
     path(
         "<str:pk>/<str:task_pk>/skip/",
         SkipLessonTaskView.as_view(),
-        name="skip_task",
+        name="task_skip",
     ),
 ]
