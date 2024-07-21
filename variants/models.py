930e2665346a02469c8f624134f7faeeb4a2d247
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-22 00:49:00 +0300

    feat: added new field to generated variant - complexity

--- variants/models.py
+++ variants/models.py
@@ -1,4 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import (
+    MaxValueValidator,
+    MinValueValidator,
+    validate_integer,
+)
 from django.db import models
 from django.utils import timezone
 
@@ -97,6 +102,9 @@ class GeneratedUserVariant(BaseModel):
         verbose_name_plural = "Сгенерированные варианты"
 
     title = models.CharField(max_length=255)
+    complexity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(3)]
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tasks = models.ManyToManyField(UserVariantTask)
     started_at = models.DateTimeField(null=True)
