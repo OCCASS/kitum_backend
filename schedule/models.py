a0be6d680dcbc252ba276d14801981afac581aad
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-28 01:54:53 +0300

    refactor: removed user schedule config

--- schedule/models.py
+++ schedule/models.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
@@ -6,17 +5,6 @@ from django.db import models
 from core.models import BaseModel
 
 User = get_user_model()
-
-
-class UserScheduleConfig(BaseModel):
-    """Модель конфигурации расписания для пользователя"""
-
-    class Meta:
-        db_table = "user_schedule_config"
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    week_day_1 = models.IntegerField(default=settings.DEFAULT_WEEK_DAY_1)
-    week_day_2 = models.IntegerField(default=settings.DEFAULT_WEEK_DAY_2)
 
 
 class Holiday(models.Model):
