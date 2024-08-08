2766f648c6ba924364bc0a70523b5c8efb25bb89
OCCCAS <pavlov.timur556@yandex.ru>
2024-08-08 09:57:04 +0300

    fix: handler404 response status changed to 404 and handler500 set to 500

--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.http import JsonResponse
 
 
 def handler404(request, *args, **kwargs):
-    return JsonResponse({"detail": "Not found."})
+    return JsonResponse({"detail": "Not found."}, status=404)
 
 
 def handler500(request, *args, **kwargs):
-    return JsonResponse({"detail": "Server error occurred."})
+    return JsonResponse({"detail": "Server error occurred."}, status=500)
